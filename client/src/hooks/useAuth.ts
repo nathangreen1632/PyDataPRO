1f67dc861ee70a1c7e7594a6a618ea38b7d9950e
Nathan Green <ngreen1632@gmail.com>
2025-05-13 16:33:22 -0500

    Add useAuth hook for accessing AuthContext
    This custom hook simplifies the process of accessing AuthContext. It ensures consistent and reusable logic for authentication-related operations across the app.

---
+++ client/src/hooks/useAuth.ts
@@ -0,0 +1,6 @@
+import { useAuthContext } from '../context/AuthContext';
+import type { AuthContextType } from '../context/AuthContext';
+
+export const useAuth = (): AuthContextType => {
+  return useAuthContext();
+};
