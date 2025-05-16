ae6019d388753f02fb4d14c942aacd5546dd6d79
Nathan Green <ngreen1632@gmail.com>
2025-05-16 09:34:05 -0500

    Add CareerPathPage route to App component
    Introduced a new route for the CareerPathPage component in the app. This allows users to navigate to the '/career-path' path and access the corresponding page content.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import Login from "./pages/Login.tsx";
 import Register from "./pages/Register.tsx";
 import { Profile } from "./pages/Profile.tsx";
 import Home  from "./pages/Home.tsx";
+import CareerPathPage from "./pages/CareerPathPage.tsx";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/profile" element={<div className="max-w-4xl mx-auto"><Profile /></div>} />
           <Route path="/interview" element={<div className="max-w-4xl mx-auto"><InterviewGenerator /></div>} />
 
+          <Route path="/career-path" element={<div className="max-w-4xl mx-auto"><CareerPathPage /></div>} />
           <Route path="/analytics" element={<div className="max-w-4xl mx-auto"><ProfileAnalyticsPage /></div>} />
           <Route path="/login" element={<div className="max-w-4xl mx-auto"><Login /></div>} />
           <Route path="/register" element={<div className="max-w-4xl mx-auto"><Register /></div>} />
